370c7a0dc66c151f76f20bc6e819db99e2329ea3
Abigael Sila <abigaelkalundesila@gmail.com>
2025-07-31 02:33:01 +0300

    changed port to 5174

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ command }) => {
   return {
     plugins: [react()],
-    // This 'base' option tells Vite where your assets will be served from.
-    // For GitHub Pages at https://username.github.io/repo-name/, it's '/repo-name/'.
-    // For local development, it should be '/'.
     base: command === 'build' ? '/Abigael-Portfolio-Website/' : '/',
+    server: { // Add this server object
+      port: 5174, // Set the desired port here
+    },
   };
 });
