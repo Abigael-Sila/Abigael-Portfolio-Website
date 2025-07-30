edb4871fb7049cac1d531bdced7f1bebad4fbc8b
Abigael Sila <abigaelkalundesila@gmail.com>
2025-07-31 02:58:26 +0300

    fixing blank screen

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ command }) => {
-  return {
-    plugins: [react()],
-    base: command === 'build' ? '/Abigael-Portfolio-Website/' : '/',
-    server: { // Add this server object
-      port: 5174, // Set the desired port here
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
+  base: '/Abigael-Portfolio-Website/', 
 });
